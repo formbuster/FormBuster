9826c02d627bd77259b93bfde676dc1a9ac2eb24
unknown <gdolphy2015@my.fit.edu>
2018-09-07 22:38:42 -0400

    connected firebase to angular project

--- FormBuster/src/environments/environment.ts
+++ FormBuster/src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    apiKey: 'AIzaSyD5gttM9fq--1BIbLtn79UbtXrMlf56oAA',
+    authDomain: 'formbuster-73603.firebaseapp.com',
+    databaseURL: 'https://formbuster-73603.firebaseio.com',
+    projectId: 'formbuster-73603',
+    storageBucket: 'formbuster-73603.appspot.com',
+    messagingSenderId: '886328297024'
+  }
 };
 
 /*
