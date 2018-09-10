6370d60347ba95d8e8f23552dcf6c4265b7b4fdf
unknown <gdolphy2015@my.fit.edu>
2018-09-09 20:18:39 -0400

    added in login screen, returns a dialog box with input

---
+++ FormBuster/src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    window.alert('email: ' + this.loginForm.controls.email.value + ' \n password: ' + this.loginForm.controls.password.value);
+  }
+}
