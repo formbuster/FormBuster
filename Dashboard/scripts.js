e365ba56bad17e962f62b9ba40c0c6df24a2235d
gdolphy2015 <gdolphy2015@my.fit.edugit config --global user.name gdolphy2015git config --glocal --listgit config --global user.name gdolphy2015git config --global user.email gdolphy2015@my.fit.edu>
2018-10-12 17:51:28 -0400

    Made more changes to design

--- Dashboard/scripts.js
+++ Dashboard/scripts.js
@@ -10,3 +10,23 @@ function w3_toggle() {
     document.getElementById("openNav").style.display = "inline-block";
   }
 }
+
+function check() {
+  console.log(window.innerWidth + "px");
+  if (window.innerWidth <= 933) {
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+
+    if (window.innerWidth <= 600) {
+
+    }
+  } else {
+    document.getElementById("main").style.marginLeft = "20%";
+    document.getElementById("mySidebar").style.width = "20%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = "none";
+  }
+
+
+}
