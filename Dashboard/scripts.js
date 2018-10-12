801f5a78d8d0b76f47a7b8823e0c4f1f919fa8ee
gdolphy2015 <gdolphy2015@my.fit.edugit config --global user.name gdolphy2015git config --glocal --listgit config --global user.name gdolphy2015git config --global user.email gdolphy2015@my.fit.edu>
2018-10-11 20:31:03 -0400

    starting

---
+++ Dashboard/scripts.js
@@ -0,0 +1,12 @@
+function w3_toggle() {
+  var toggle = document.getElementById("mySidebar");
+  if (toggle.style.display === "none") {
+    document.getElementById("main").style.marginLeft = "20%";
+    document.getElementById("mySidebar").style.width = "20%";
+    document.getElementById("mySidebar").style.display = "block";
+  } else {
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+  }
+}
