414db59bd65ca3d8bef78ff00e16371611089329
McNels <smcnels@me.com>
2018-10-17 14:54:41 -0400

    Update dash.js

--- js/dash.js
+++ js/dash.js
@@ -1,23 +1,23 @@
-var homeApp = {};
+var mainApp = {};
 (function(){
   var mainContainer = document.getElementById("main_container");
 
-//   var signOut =  function(){
-//         firebase.auth().signOut().then(function(){
-//             console.log('success');
-//             window.location.replace("http://pseudotracks.me/");
-//         },function(){})
-//     }
+  var signOut =  function(){
+        app_firebase.auth().signOut().then(function(){
+            console.log('success');
+            window.location.replace("http://pseudotracks.me/");
+        },function(){})
+    }
   
   var init = function(){
-    firebase.auth().onAuthStateChanged(function(user) {
+    app_firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in
             console.log("stay");
-            mainContainer.style.display = "";
+            //mainContainer.style.display = "";
         } else {
             // redirect to login page
-            mainContainer.style.display = "none";
+            //mainContainer.style.display = "none";
             console.log("redirect");
             window.location.replace("http://pseudotracks.me/");
         }
@@ -26,5 +26,5 @@ var homeApp = {};
     
   init();
   
-//   homeApp.logout = signOut;
+   mainApp.logout = signOut;
 })();
