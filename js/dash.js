c9492af626f7b67cd61b84a9b8461fc6c51e2b90
McNels <smcnels@me.com>
2018-10-17 15:23:26 -0400

    Update dash.js

--- js/dash.js
+++ js/dash.js
@@ -1,13 +1,13 @@
 var mainApp = {};
 (function(){
-  var mainContainer = document.getElementById("main_container");
+  //var mainContainer = document.getElementById("main_container");
 
-  var signOut =  function(){
-        app_firebase.auth().signOut().then(function(){
-            console.log('success');
-            window.location.replace("http://pseudotracks.me/");
-        },function(){})
-    }
+//   var signOut =  function(){
+//         app_firebase.auth().signOut().then(function(){
+//             console.log('success');
+//             window.location.replace("http://pseudotracks.me/");
+//         },function(){})
+//     }
   
   var init = function(){
     app_firebase.auth().onAuthStateChanged(function(user) {
@@ -19,7 +19,7 @@ var mainApp = {};
             // redirect to login page
             //mainContainer.style.display = "none";
             console.log("redirect");
-            window.location.replace("http://pseudotracks.me/");
+            //window.location.replace("http://pseudotracks.me/");
         }
     });
   }
