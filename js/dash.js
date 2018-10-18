a8b6dcb5b6a1ca6ec61203b9a4029c1c43180ddf
McNels <smcnels@me.com>
2018-10-17 23:27:44 -0400

    Update dash.js

--- js/dash.js
+++ js/dash.js
@@ -1,40 +1,22 @@
-var mainApp = {};
-(function(){
-  //var mainContainer = document.getElementById("main_container");
-
-//   var signOut =  function(){
-//         app_firebase.auth().signOut().then(function(){
-//             console.log('success');
-//             window.location.replace("http://pseudotracks.me/");
-//         },function(){})
-//     }
+var homeApp = {};
+(function() {
+  var firebase = app_firebase;
   var uid = null;
-  
-    app_firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            // User is signed in
-            console.log("stay");
-            //mainContainer.style.display = "";
+
+  firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+          // User is signed in
           uid = user.uid;
-          console.log(uid);
-        } else {
-            // redirect to login page
+      } else {
+          // redirect to login page
           uid = null;
-          console.log(uid);
-            //mainContainer.style.display = "none";
-            console.log("redirect");
-            //window.location.replace("http://pseudotracks.me/");
-        }
-    });
-  //}
- 
- function signOut() {
-    console.log("trying to log out");
-    app_firebase.auth().signOut();
-    console.log("logged out");
+          window.location.replace('https://auth.formbuster.me/');
+      }
+  });
+
+  function logOut(){
+    firebase.auth().signOut();
   }
-    
-  //init();
-  document.getElementById("signout").onclick = signOut;
-   //mainApp.logout = signOut();
-})();
+
+  homeApp.logOut = logOut;
+})()
