03b3f19001beef41d281645327ebfe5d65201ddf
unknown <gdolphy2015@my.fit.edu>
2018-10-24 01:02:37 -0400

    Added in missing functions to change views between Dashboard and Students

--- Dashboard/js/staff.js
+++ Dashboard/js/staff.js
@@ -1,3 +1,15 @@
 function getDisplayName() {
     document.getElementById("display-name").innerHTML = getUserName();
 }
+
+function showDashboard() {
+    document.getElementById("dashboard-contents").style.display = "block";
+    document.getElementById("students-list").style.display = "none";
+
+}
+
+function showStudentsList() {
+    document.getElementById("dashboard-contents").style.display = "none";
+    document.getElementById("students-list").style.display = "block";
+
+}
