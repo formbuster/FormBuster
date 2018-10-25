c89a5095b854b2f3f327f51f2dbc983da295529b
unknown <gdolphy2015@my.fit.edu>
2018-10-24 22:57:08 -0400

    Added visible clicked menu option, and added in add a form pop up

--- Dashboard/js/stu.js
+++ Dashboard/js/stu.js
@@ -4,18 +4,36 @@ tabs.
  */
 
 function showDashboard() {
+    //highlight only the dashboard tag, because it is selected.
+    document.getElementById("dashboard").className = "w3-bar-item w3-button w3-hover-theme w3-large w3-text-theme-red w3-background";
+    document.getElementById("forms").className = "w3-bar-item w3-button w3-hover-theme w3-large";
+    document.getElementById("history").className = "w3-bar-item w3-button w3-hover-theme w3-large";
+
+    //only show the dashboard, because it is selected.
     document.getElementById("dashboard-contents").style.display = "block";
     document.getElementById("forms-list").style.display = "none";
     document.getElementById("history-list").style.display = "none";
 }
 
 function showFormsList() {
+    //highlight only the forms tag, because it is selected.
+    document.getElementById("dashboard").className = "w3-bar-item w3-button w3-hover-theme w3-large";
+    document.getElementById("forms").className = "w3-bar-item w3-button w3-hover-theme w3-large w3-text-theme-red w3-background";
+    document.getElementById("history").className = "w3-bar-item w3-button w3-hover-theme w3-large";
+
+    //only show the forms, because it is selected.
     document.getElementById("forms-list").style.display = "block";
     document.getElementById("dashboard-contents").style.display = "none";
     document.getElementById("history-list").style.display = "none";
 }
 
 function showHistoryList() {
+    //highlight only the history tag, because it is selected.
+    document.getElementById("dashboard").className = "w3-bar-item w3-button w3-hover-theme w3-large";
+    document.getElementById("history").className = "w3-bar-item w3-button w3-hover-theme w3-large w3-text-theme-red w3-background";
+    document.getElementById("forms").className = "w3-bar-item w3-button w3-hover-theme w3-large";
+
+    //only show the history, because it is selected.
     document.getElementById("history-list").style.display = "block";
     document.getElementById("forms-list").style.display = "none";
     document.getElementById("dashboard-contents").style.display = "none";
