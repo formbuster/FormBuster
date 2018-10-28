0ad5ef946bf357aa115fa6cc679f1fdf2ef24727
Raphael Setin <raphael-setin@hotmail.com>
2018-10-28 16:27:13 -0400

    Internal functions that help the computation of Dates were added.

--- Dashboard/js/general.js
+++ Dashboard/js/general.js
@@ -29,5 +29,39 @@ function check() {
     }
 }
 
+// Due date will be computed from the formName
+function getFormDueDate (formName) {
+    return pawsDB.collection("formDeadlines").doc(formName).get().then(function(doc) {
+        if (doc.exists) {
+            const docData = doc.data();
+            return getFormattedDate(docData.deadline.toDate());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+}
 
+function getDateFromTimestamp (timestamp) {
+    const month = timestamp.substring(0, 2);
+    const day = timestamp.substring(2, 4);
+    const year = timestamp.substring(4, 8);
+    const hour = timestamp.substring(8, 10);
+    const minute = timestamp.substring(10, 12);
+    const second = timestamp.substring(12, 14);
+
+    const date = new Date (year, month - 1, day, hour, minute, second, 0);
+    return date;
+}
+
+function getFormattedDate (date) {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear().toString().substr(-2);
+
+    const fullDate = month + "/" + day + "/" + year;
+    return fullDate;
+}
 
