88f2bd9646c1144bc03db7bc71b84d3c60528c5f
Raphael Setin <raphael-setin@hotmail.com>
2018-10-28 16:28:41 -0400

    Added CSS style for tooltips and also implemented the display of data of FormBuster database to the student's dashboard.

--- Dashboard/js/stu.js
+++ Dashboard/js/stu.js
@@ -48,61 +48,112 @@ function setUpDashboard() {
 }
 
 function getDisplayName() {
-    document.getElementById("display-name").innerHTML = getUserName();
+    const userName = getUserName();
+
+    pawsDB.collection("users").doc(userName).get().then(function(doc) {
+        if (doc.exists) {
+            const docData = doc.data();
+            document.getElementById("display-name").innerHTML = docData.name.first + " " + docData.name.middle + " " + docData.name.last;
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
 }
+
 /*
 This function's purpose is to generate the in-process forms view for a student on their dashboard.
  */
 function getInProgressForm() {
-    var txt = '{"form_name":"Registration Form", "needed_approvals": 3, "approvals":1, "submission_date":"09-09-2018", "due_date":"09-09-2018", "student":"Student, Test"}';
-    var obj = JSON.parse(txt);
-    console.log(obj.form_name + " " + obj.approvals + " " + obj.submission_date + " " + obj.due_date + " " + obj.student);
+    const userName = getUserName();
 
-    //swap out 4 for the number of in progress forms for a student
-    for (let nam_of_form_occurrences = 0; nam_of_form_occurrences < 4; nam_of_form_occurrences++) {
-        var main_div = document.createElement("div");
-        main_div.className = "w3-white w3-button w3-block w3-leftbar w3-border-theme w3-round-xlarge w3-margin-bottom\t";
-        //make a new div with class name, and append it to main div
-        var first_nested_div = document.createElement("div");
-        first_nested_div.className = "w3-left";
-        main_div.appendChild(first_nested_div);
-        //make an h3 tag, make text, append text to h3 tag, append h3 tag to first_nested_div
-        var h3_form_name = document.createElement('h3');
-        var txt_form_name = document.createTextNode(obj.form_name);
-        h3_form_name.appendChild(txt_form_name);
-        first_nested_div.appendChild(h3_form_name);
-        //generate check marks
-        for (i = 1; i <= obj.needed_approvals; i++) {
-            var span_check_mark = document.createElement('span');
-            //Green check mark since it has already been approved..
-            if (i <= obj.approvals) {
-                span_check_mark.className = "w3-left w3-badge w3-green w3-large";
-            } else { // has not been approved so use the grey check mark style
-                span_check_mark.className = "w3-left w3-margin-left w3-badge w3-grey w3-large";
+    formDB.collection("users").doc(userName).collection("inProgressForms").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+
+            const docData = doc.data();
+            const formName = doc.id.toString().split('_')[0];
+            const submDate = getFormattedDate(getDateFromTimestamp(doc.id.toString().split('_')[1]));
+            const dueDatePromise = getFormDueDate(formName);
+            const approvals = docData.approvals;
+
+            var main_div = document.createElement("div");
+            main_div.className = "w3-white w3-button w3-block w3-leftbar w3-border-theme w3-round-xlarge w3-margin-bottom\t";
+            //make a new div with class name, and append it to main div
+            var first_nested_div = document.createElement("div");
+            first_nested_div.className = "w3-left";
+            main_div.appendChild(first_nested_div);
+            //make an h3 tag, make text, append text to h3 tag, append h3 tag to first_nested_div
+            var h3_form_name = document.createElement('h3');
+            var txt_form_name = document.createTextNode(formName);
+            h3_form_name.appendChild(txt_form_name);
+            first_nested_div.appendChild(h3_form_name);
+            //generate check marks
+            for (i = 0; i < approvals.length; i++) {
+                console.assert(approvals[i].status != false, {message: "Assertion failed. We cannot have a declined approval in 'inProgressForms'."});
+                var span_check_mark = document.createElement('span');
+                var span_check_tooltip = document.createElement('span');
+                span_check_tooltip.className = "tooltiptext";
+
+                pawsDB.collection("users").doc(approvals[i].tracksID).get().then(function(doc) {
+                    if (doc.exists) {
+                        const docData = doc.data();
+                        var tooltip_txt = document.createTextNode(docData.name.first + " " + docData.name.last);
+                        span_check_tooltip.appendChild(tooltip_txt);
+                    } else {
+                        // doc.data() will be undefined in this case
+                        console.log("No such document!");
+                    }
+                }).catch(function(error) {
+                    console.log("Error getting document:", error);
+                });
+
+                if (approvals[i].status == null) {
+                    // has not been approved so use the grey check mark style
+                    span_check_mark.className = "w3-left w3-margin-left w3-badge w3-grey w3-large";
+                } else if (approvals[i].status == true) {
+                    //Green check mark since it has already been approved..
+                    span_check_mark.className = "w3-left w3-badge w3-green w3-large";
+                }
+                var txt_form_name = document.createTextNode("✓");
+                span_check_mark.appendChild(txt_form_name);
+                span_check_mark.appendChild(span_check_tooltip);
+
+
+                var div_bubble = document.createElement('div');
+                div_bubble.className = "tooltip";
+                div_bubble.appendChild(span_check_mark);
+
+
+                first_nested_div.appendChild(div_bubble);
             }
-            var txt_form_name = document.createTextNode("✓");
-            span_check_mark.appendChild(txt_form_name);
-            first_nested_div.appendChild(span_check_mark);
-        }
-        var second_nested_div = document.createElement("div");
-        second_nested_div.className = "w3-right"
-        var h4_submission_date = document.createElement('h3');
-        var submission_date_txt = document.createTextNode("Submission Date: " + obj.submission_date);
-        h4_submission_date.appendChild(submission_date_txt);
-        second_nested_div.appendChild(h4_submission_date);
-        var h4_due_date = document.createElement('h3');
-        var due_date_txt = document.createTextNode("Due Date: " + obj.due_date);
-        h4_due_date.appendChild(due_date_txt);
-        second_nested_div.appendChild(h4_due_date);
+            var second_nested_div = document.createElement("div");
+            second_nested_div.className = "w3-right";
+            var h4_submission_date = document.createElement('h3');
+            var submission_date_txt = document.createTextNode("Submission Date: " + submDate);
+            h4_submission_date.appendChild(submission_date_txt);
+            second_nested_div.appendChild(h4_submission_date);
+            var h4_due_date = document.createElement('h3');
 
-        main_div.appendChild(second_nested_div);
+            dueDatePromise.then(function(result) {
+                var due_date_txt = document.createTextNode("Due Date: " + result);
+                h4_due_date.appendChild(due_date_txt);
+                second_nested_div.appendChild(h4_due_date);
+                main_div.appendChild(second_nested_div);
+            });
 
-        /*Todo: finish function call for on click, (see below)
-        Todo: lines 29-41 should be used above for the in-progress form information.
-        */
-        main_div.onclick = function(){console.log("clicked")};
-        document.getElementById("transit_form_list").appendChild(main_div);
-    }
+
+            /*Todo: finish function call for on click, (see below)
+            Todo: lines 29-41 should be used above for the in-progress form information.
+            */
+            main_div.onclick = function(){console.log("clicked")};
+            document.getElementById("transit_form_list").appendChild(main_div);
+        })
+    }).catch(function(error) {
+        console.log("Error getting documents (querySnapshot): ", error);
+    });
 }
 
 /*
