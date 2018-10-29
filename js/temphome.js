d2b5ed644d7de41a4add0703eed8ad233e0a2797
mcnels sylvestre <smcnels@me.com>
2018-10-28 20:55:58 -0400

    compiling...

---
+++ js/temphome.js
@@ -0,0 +1,22 @@
+var homeApp = {};
+(function() {
+    var firebase = app_firebase;
+    var uid = null;
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in
+            uid = user.uid;
+        } else {
+            // redirect to login page
+            uid = null;
+            window.location.replace('../src/templogin.html');
+        }
+    });
+
+    function logOut(){
+        firebase.auth().signOut();
+    }
+
+    homeApp.logOut = logOut;
+})()
