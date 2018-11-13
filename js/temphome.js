80e7a44208ddc3e74a06af1ea282a4e91d525608
unknown <gdolphy2015@my.fit.edu>
2018-11-13 16:15:45 -0500

    should fix my error

--- js/temphome.js
+++ js/temphome.js
@@ -1,17 +1,9 @@
 var homeApp = {};
 (function() {
-    // Initialize PseudoTRACKS Firebase
-    pseudoTRACKSApp = firebase.initializeApp({
-        apiKey: "AIzaSyAWosl7w3NfRpAYL-j1XWLDZujl8GpO-KA",
-        authDomain: "pseudotracks-bb89e.firebaseapp.com",
-        databaseURL: "https://pseudotracks-bb89e.firebaseio.com",
-        projectId: "pseudotracks-bb89e",
-        storageBucket: "pseudotracks-bb89e.appspot.com",
-        messagingSenderId: "369787432909"
-    }, "pseudoTRACKS");
+    var firebase = app_firebase;
     var uid = null;
 
-    pseudoTRACKSApp.auth().onAuthStateChanged(function(user) {
+    firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in
             uid = user.uid;
@@ -27,4 +19,4 @@ var homeApp = {};
     }
 
     homeApp.logOut = logOut;
-})();
+})()
