964faf167eecf3a67c770c3904f569f5bc8e2df1
McNels <smcnels@me.com>
2018-11-26 11:51:57 -0500

    test

--- js/templogin.js
+++ js/templogin.js
@@ -1,4 +1,4 @@
-(function() {
+// (function() {
     // Initialize the FirebaseUI Widget using Firebase.
     var ui = new firebaseui.auth.AuthUI(firebase.auth());
     var uiConfig = {
@@ -27,4 +27,4 @@
 
     // The start method will wait until the DOM is loaded.
     ui.start('#firebaseui-auth-container', uiConfig);
-})()
+// })()
