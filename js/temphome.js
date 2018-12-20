edb3c85aaef570e8facf93bf73242507e33f295c
McNels Sylvestre <smcnels@me.com>
2018-12-20 11:30:18 -0500

    Update temphome.js

--- js/temphome.js
+++ js/temphome.js
@@ -7,9 +7,40 @@ var homeApp = {};
     firebase.auth().onAuthStateChanged(function(user) {
         genUserEmail = user.email;
         console.log(genUserEmail);
+        
         if (user) {
             // User is signed in
             uid = user.uid;
+            
+            function redirectUser() {
+            //   var signedIn = user.email;
+              console.log(genUserEmail);
+
+              pawsDB.collection("users").doc(genUserEmail.toString().substring(0, str.indexOf("@"))).get().then(function(doc) {
+                if (doc.exists) {
+                  const docData = doc.data();
+                  const usertype = docData.userType;
+
+                  if (usertype == "Student") {
+                    window.location.replace("../src/stu.html");
+                  } else if (usertype == "Faculty") {
+                    window.location.replace("../src/faculty.html");
+                  } else if (usertype == "Staff") {
+                    window.location.replace("../src/staff.html");
+                  } else {
+                    window.location.replace("../src/coord.html");
+                  }
+
+                } else {
+                  // doc.data() will be undefined in this case
+                  console.log("No such document!");
+                }
+              }).catch(function(error) {
+                console.log("Error type:", error);
+              });
+
+
+            }
         } else {
             // redirect to login page
             uid = null;
