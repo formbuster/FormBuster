f10870366da15aa34ca224f46df5ec190cc2942d
McNels Sylvestre <smcnels@me.com>
2018-12-20 12:07:32 -0500

    Update temphome.js

--- js/temphome.js
+++ js/temphome.js
@@ -23,28 +23,30 @@ var homeApp = {};
             // User is signed in
             uid = user.uid;
             
-            pawsDB.collection("users").doc(user.email.toString().substring(0, str.indexOf("@"))).get().then(function(doc) {
-                if (doc.exists) {
-                  const docData = doc.data();
-                  const usertype = docData.userType;
+            document.getElementById('pageLoaded').onload = function () {
+                pawsDB.collection("users").doc(user.email.toString().substring(0, str.indexOf("@"))).get().then(function(doc) {
+                    if (doc.exists) {
+                      const docData = doc.data();
+                      const usertype = docData.userType;
 
-                  if (usertype == "Student") {
-                    window.location.replace("../src/stu.html");
-                  } else if (usertype == "Faculty") {
-                    window.location.replace("../src/faculty.html");
-                  } else if (usertype == "Staff") {
-                    window.location.replace("../src/staff.html");
-                  } else {
-                    window.location.replace("../src/coord.html");
-                  }
+                      if (usertype == "Student") {
+                        window.location.replace("../src/stu.html");
+                      } else if (usertype == "Faculty") {
+                        window.location.replace("../src/faculty.html");
+                      } else if (usertype == "Staff") {
+                        window.location.replace("../src/staff.html");
+                      } else {
+                        window.location.replace("../src/coord.html");
+                      }
 
-                } else {
-                  // doc.data() will be undefined in this case
-                  console.log("No such document!");
-                }
-              }).catch(function(error) {
-                console.log("Error type:", error);
-              });
+                    } else {
+                      // doc.data() will be undefined in this case
+                      console.log("No such document!");
+                    }
+                  }).catch(function(error) {
+                    console.log("Error type:", error);
+                  });
+            }   
             
             
             
